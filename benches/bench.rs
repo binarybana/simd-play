0f41b2a5f613e9d4bdf860b658a07178018d725b
Jason Knight <Jason@JasonKnight.us>
2018-09-09 12:17:36 -0700

    64 size for future 8 blocking

--- benches/bench.rs
+++ benches/bench.rs
@@ -5,14 +5,14 @@ use criterion::Criterion;
 
 use simd_play::*;
 
-const M: usize = 20;
-const N: usize = 40;
-const K: usize = 20;
+const M: usize = 64;
+const N: usize = 64;
+const K: usize = 64;
 
 fn criterion_benchmark(c: &mut Criterion) {
     let a_mat = get_rand_matrix(M,N);
     let b_mat = get_rand_matrix(N,K);
-    c.bench_function("matmul 20", move |b| b.iter(|| matmul(&a_mat, &b_mat)));
+    c.bench_function("matmul 64", move |b| b.iter(|| matmul(&a_mat, &b_mat)));
 }
 
 fn criterion_benchmark2(c: &mut Criterion) {
@@ -21,7 +21,7 @@ fn criterion_benchmark2(c: &mut Criterion) {
     use ndarray::prelude::*;
     let a_nd = Array::from_vec(a_mat.data).into_shape((M, N)).unwrap();
     let b_nd = Array::from_vec(b_mat.data).into_shape((N, K)).unwrap();
-    c.bench_function("matmul_ndarray 20", move |b| b.iter(|| a_nd.dot(&b_nd)));
+    c.bench_function("matmul_ndarray 64", move |b| b.iter(|| a_nd.dot(&b_nd)));
 }
 
 criterion_group!(benches, criterion_benchmark, criterion_benchmark2);
--- src/lib.rs
+++ src/lib.rs
@@ -20,9 +20,16 @@ pub fn get_rand_matrix(m: usize, n: usize) -> Mat {
 
 pub fn matmul(a: &Mat, b: &Mat) -> Mat {
     let mut result = Mat{data: vec![0.0; a.height * b.width], height: a.height, width: b.width};
+    // let res = result.data.as_mut_ptr();
     for row in 0..a.height {
         for col in 0..b.width {
             for k in 0..a.width {
+                // for block_row in 0..8 {
+                // unsafe {
+                // *result.data.get_unchecked_mut(row*result.width + col) +=
+                //     *a.data.get_unchecked(row*a.width + k) *
+                //     *b.data.get_unchecked(k*b.width + col);
+                // }
                 result.data[row*result.width + col] += a.data[row*a.width + k] * b.data[k*b.width + col];
             }
         }
@@ -35,14 +42,17 @@ mod tests {
     use super::*;
     #[test]
     fn test() {
-        let a = get_rand_matrix(2,3);
-        let b = get_rand_matrix(3,2);
+        const M: usize = 8;
+        const K: usize = 8;
+        const N: usize = 8;
+        let a = get_rand_matrix(M,K);
+        let b = get_rand_matrix(K,N);
         let c = matmul(&a, &b);
 
         use ndarray::prelude::*;
-        let a_nd = Array::from_vec(a.data).into_shape((2,3)).unwrap();
-        let b_nd = Array::from_vec(b.data).into_shape((3,2)).unwrap();
-        let c_nd = Array::from_vec(c.data).into_shape((2,2)).unwrap();
+        let a_nd = Array::from_vec(a.data).into_shape((M,K)).unwrap();
+        let b_nd = Array::from_vec(b.data).into_shape((K,N)).unwrap();
+        let c_nd = Array::from_vec(c.data).into_shape((M,N)).unwrap();
 
         let c_nd_true = a_nd.dot(&b_nd);
         println!("True: {}\nc: {}", c_nd_true, c_nd);
